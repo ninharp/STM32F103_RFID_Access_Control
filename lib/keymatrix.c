cc8354182c8b876d345de2de00a052f80a1cd10e
Michael Sauer <sauer.uetersen@gmail.com>
2016-11-18 17:52:09 +0100

    Added KeyMatrix Module and some small changes
    Added files for KeyMatrix Library (keymatrix.c/h)
    Auto Version Incrementing was now activated

---
+++ lib/keymatrix.c
@@ -0,0 +1,24 @@
+/*
+ * keymatrix.c
+ *
+ *  Created on: 18.11.2016
+ *      Author: Michael Sauer <sauer.uetersen@gmail.com>
+ */
+
+#ifndef LIB_KEYMATRIX_C_
+#define LIB_KEYMATRIX_C_
+
+/* Includes ------------------------------------------------------------------*/
+#include "keymatrix.h"
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+uint8_t columns;
+uint8_t rows;
+/* Private function prototypes -----------------------------------------------*/
+
+#endif /* LIB_KEYMATRIX_C_ */
+
+
+
